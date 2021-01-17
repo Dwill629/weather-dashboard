8f23a49f3c3995ebce55c5dd821c2c249a375a24
Devon <Dwill629@gmail.com>
2021-01-17 16:26:53 -0500

    added locations for left search for city column in js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,9 @@
 var apiKey = "e2ff381169e010bdeac6fd79eab4293a";
 var searchBtn = $(".searchBtn");
 var searchInput = $(".searchInput");
+
+//  Grabbing search for city/Left column locations
+var cityNameEl = $(".cityName");
+var currentDateEl = $(".currentDate");
+var weatherIconEl = $(".weatherIcon");
+var searchHistoryEl = $(".historyItems");
